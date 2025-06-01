31f569b6b150b80dd747f2be59e3c385d32e4136
santiago-chica <legarzon97@gmail.com>
2025-05-31 23:46:26 -0500

    Update album-card.tsx

--- src/components/music/album-card.tsx
+++ src/components/music/album-card.tsx
@@ -41,7 +41,7 @@ export function AlbumCard({ album }: AlbumCardProps) {
               size="icon"
               className="absolute bottom-2 right-2 bg-background/70 hover:bg-primary hover:text-primary-foreground text-primary-foreground opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-full h-10 w-10"
               onClick={handlePlayAlbum}
-              aria-label={`Play ${album.title}`}
+              aria-label={`Reproducir ${album.title}`}
             >
               <PlayCircle size={28} />
             </Button>
